3a895d1d40861f5c125c00b31e6a39231d2e0dd6
Arianna Masciolini <uzkamascio@gmail.com>
2019-03-15 09:33:31 +0100

    Auto stash before merge of "cleanup" and "origin/cleanup"

--- run_docker.sh
+++ run_docker.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-if [ "$1" == 'gpu' ]
-then
-    docker build -t keras-project:gpu .
-    sudo docker run --runtime=nvidia --rm -it -u $(id -u):$(id -g) -v $(pwd):/data keras-project:gpu bash
-elif [ "$1" == 'cpu' ]
-then
-    docker build -t keras-project .
-    sudo docker run --rm -it -u $(id -u):$(id -g) -v $(pwd):/data keras-project bash
-else
-    echo "ERROR: missing parameter (either gpu or cpu)"
-fi
-
+docker build -t keras-project .
+docker run -it -u $(id -u):$(id -g) -v $(pwd):/data keras-project bash
